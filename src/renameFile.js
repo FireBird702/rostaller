15138eebcb00c1c004bf1e359990b42156a86862
FireBird702 <112522765+FireBird702@users.noreply.github.com>
2024-08-17 05:37:29 +0200

    retry on rename failure

--- src/renameFile.js
+++ src/renameFile.js
@@ -1,18 +1,36 @@
 import graceful_fs from "graceful-fs"
 import { setTimeout } from "timers/promises"
+import { red } from "./output/colors.js"
 
 const rename = graceful_fs.rename
 
-export async function renameFile(oldPath, newPath) {
-	var completed = false
+const maxAttempts = 5
 
+function renameWithRetry(oldPath, newPath, data) {
 	rename(oldPath, newPath, (err) => {
-		if (err)
-			throw `Failed to rename [${oldPath}] to [${newPath}]`
+		if (err) {
+			data.attempts += 1
 
-		completed = true
+			if (data.attempts < maxAttempts)
+				renameAttempt(oldPath, newPath, data)
+			else
+				console.error(red(`Failed to rename [${oldPath}] to [${newPath}]`))
+
+			return
+		}
+
+		data.completed = true
 	})
+}
 
-	while (!completed)
+export async function renameFile(oldPath, newPath) {
+	var data = {
+		completed: false,
+		attempts: 0
+	}
+
+	renameWithRetry(oldPath, newPath, data)
+
+	while (!data.completed && data.attempts < maxAttempts)
 		await setTimeout(100)
 }
